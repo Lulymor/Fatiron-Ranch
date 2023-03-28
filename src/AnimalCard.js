ed19ee866d179886abbee9ec83455b9d645aaf1c
+ <+>
2023-03-28 13:09:19 -0400

    deploy

--- src/AnimalCard.js
+++ src/AnimalCard.js
@@ -1,22 +1,20 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-
-//I can mark a plant as sold out 
-
-function AnimalCard({species, age, name}) {
+function AnimalCard({ species, age, name }) {
   const [isInStock, setIsInStock] = useState(true);
 
-  function handleStock(){
-    setIsInStock((isInStock) => !isInStock)
+  function handleStock() {
+    setIsInStock((isInStock) => !isInStock);
   }
   return (
     <li className="card">
-      
       <h4>{species}</h4>
       <h4>{name}</h4>
       <p>Age:{age}</p>
       {isInStock ? (
-        <button className="primary" onClick={handleStock}>In Stock</button>
+        <button className="primary" onClick={handleStock}>
+          In Stock
+        </button>
       ) : (
         <button onClick={handleStock}>Out of Stock</button>
       )}
